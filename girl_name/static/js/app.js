1ba494fe7e0821457aa9ec5024ce5e43bd60bcb7
AleksanderJanusz <oleksanderjanusz@gmail.com>
2023-07-18 17:45:12 +0200

    js drag and drop, to do serializer and db

--- girl_name/static/js/app.js
+++ girl_name/static/js/app.js
@@ -1,8 +1,4 @@
-function switch_choises() {
-    const switch_buttons = document.querySelector('#over_buttons').children;
-    const yes = document.querySelector('#ul-yes');
-    const no = document.querySelector('#ul-no');
-    const maybe = document.querySelector('#ul-maybe');
+function switch_choises(switch_buttons, yes, no, maybe) {
 
     function change_border() {
         for (let button of switch_buttons) {
@@ -35,6 +31,44 @@ function switch_choises() {
     })
 }
 
-document.addEventListener('DOMContentLoaded', function (){
-    switch_choises();
+document.addEventListener('DOMContentLoaded', function () {
+    const switch_buttons = document.querySelector('#over_buttons').children;
+    const yes = document.querySelector('#ul-yes');
+    const no = document.querySelector('#ul-no');
+    const maybe = document.querySelector('#ul-maybe');
+    const names = document.querySelectorAll('.wide-div a');
+
+    switch_choises(switch_buttons, yes, no, maybe);
+
+    names.forEach(name => {
+        name.addEventListener('dragstart', function (event) {
+            selected_name = event.target;
+        })
+
+        Array.from(switch_buttons).forEach(button => {
+            button.addEventListener('dragover', function (event) {
+                event.preventDefault();
+            })
+            button.addEventListener('drop', function (event) {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+
+                if (this.innerText === 'Tak') {
+                    let li = document.createElement('li');
+                    console.log(selected_name)
+                    li.appendChild(selected_name);
+                    yes.appendChild(li);
+                } else if (this.innerText === 'Nie') {
+                    let li = document.createElement('li');
+                    li.appendChild(selected_name);
+                    no.appendChild(li);
+                } else {
+                    let li = document.createElement('li');
+                    li.appendChild(selected_name);
+                    maybe.appendChild(li);
+                }
+            })
+        })
+
+    })
 })
